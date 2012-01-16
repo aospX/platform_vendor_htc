8dbeb25dffde87afb09d37e227a5abfd546b801f
Rob Beane <robbeane@gmail.com>
2012-01-16 18:07:26 -0500

    add camera libs

--- ace/device-vendor-blobs.mk
+++ ace/device-vendor-blobs.mk
@@ -16,6 +16,7 @@
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
+    vendor/htc/ace/proprietary/libcamera.so:/obj/lib/libcamera.so \
     vendor/htc/ace/proprietary/libaudioalsa.so:obj/lib/libaudioalsa.so
 
 # All the blobs necessary for ace
@@ -41,4 +42,8 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/proprietary/libhtc_acoustic.so:/system/lib/libhtc_acoustic.so \
     vendor/htc/ace/proprietary/libhtc_ril.so:/system/lib/libhtc_ril.so \
     vendor/htc/ace/proprietary/libmmipl.so:/system/lib/libmmipl.so \
-    vendor/htc/ace/proprietary/libmmjpeg.so:/system/lib/libmmjpeg.so
+    vendor/htc/ace/proprietary/libmmjpeg.so:/system/lib/libmmjpeg.so \
+    vendor/htc/ace/proprietary/libcamera.so:/system/lib/libcamera.so \
+    vendor/htc/ace/proprietary/lsc_camera:/system/bin/lsc_camera \
+    vendor/htc/ace/proprietary/liboemcamera.so:/system/lib/liboemcamera.so \
+    vendor/htc/ace/proprietary/awb_camera:/system/bin/awb_camera
