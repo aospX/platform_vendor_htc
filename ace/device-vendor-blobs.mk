c345da8c1aacc336b53c0e7525bce2dc80effc49
Rob Beane <robbeane@gmail.com>
2012-03-24 14:20:33 -0400

    Move all qcom libs to vendor/qcom/proprietary

--- ace/device-vendor-blobs.mk
+++ ace/device-vendor-blobs.mk
@@ -17,8 +17,7 @@
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
     vendor/htc/ace/proprietary/libcamera.so:obj/lib/libcamera.so \
-    vendor/htc/ace/proprietary/libaudioalsa.so:obj/lib/libaudioalsa.so \
-    vendor/htc/ace/proprietary/libv8.so:obj/lib/libv8.so
+    vendor/htc/ace/proprietary/libaudioalsa.so:obj/lib/libaudioalsa.so
 
 # All the blobs necessary for ace
 PRODUCT_COPY_FILES += \
@@ -29,25 +28,13 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/proprietary/rmt_storage:/system/bin/rmt_storage \
     vendor/htc/ace/proprietary/snd3254:/system/bin/snd3254 \
     vendor/htc/ace/proprietary/AudioBTID.csv:/system/etc/AudioBTID.csv \
-    vendor/htc/ace/proprietary/eglsubAndroid.so:/system/lib/egl/eglsubAndroid.so \
-    vendor/htc/ace/proprietary/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
-    vendor/htc/ace/proprietary/libGLES_android.so:/system/lib/egl/libGLES_android.so \
-    vendor/htc/ace/proprietary/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
-    vendor/htc/ace/proprietary/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
-    vendor/htc/ace/proprietary/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \
-    vendor/htc/ace/proprietary/libgsl.so:/system/lib/libgsl.so \
-    vendor/htc/ace/proprietary/libsc-a2xx.so:/system/lib/libsc-a2xx.so \
-    vendor/htc/ace/proprietary/libC2D2.so:/system/lib/libC2D2.so \
-    vendor/htc/ace/proprietary/libOpenVG.so:/system/lib/libOpenVG.so \
     vendor/htc/ace/proprietary/libaudioalsa.so:/system/lib/libaudioalsa.so \
     vendor/htc/ace/proprietary/libgemini.so:/system/lib/libgemini.so \
     vendor/htc/ace/proprietary/libhtc_acoustic.so:/system/lib/libhtc_acoustic.so \
     vendor/htc/ace/proprietary/libhtc_ril.so:/system/lib/libhtc_ril.so \
     vendor/htc/ace/proprietary/libmmipl.so:/system/lib/libmmipl.so \
     vendor/htc/ace/proprietary/libmmjpeg.so:/system/lib/libmmjpeg.so \
-    vendor/htc/ace/proprietary/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
     vendor/htc/ace/proprietary/libcamera.so:/system/lib/libcamera.so \
-    vendor/htc/ace/proprietary/libv8.so:system/lib/libv8.so \
     vendor/htc/ace/proprietary/lsc_camera:/system/bin/lsc_camera \
     vendor/htc/ace/proprietary/liboemcamera.so:/system/lib/liboemcamera.so \
     vendor/htc/ace/proprietary/awb_camera:/system/bin/awb_camera
